b720f8f0e28f9ebcbf59e4b18128b7cdccecd931
amanjagdev <amankumarjagdev@gmail.com>
2020-08-10 18:31:26 +0530

    Updated new logos

--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,20 @@ import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { colors } from '../constants/constant'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useTheme } from '@ui-kitten/components';
 
-const Header = ({ children, myStyle,navigation }) => {
+const Header = ({ children, myStyle, navigation }) => {
+    const theme = useTheme();
     return (
-        <View style={{ ...styles.container, ...myStyle }}>
-            <TouchableOpacity onPress = {() => navigation.navigate("Splash") }>
-                <Image source={require('../assets/logo.jpg')} style={{
+        <View style={{ ...styles.container,backgroundColor: theme['background-basic-1'], ...myStyle }}>
+            <TouchableOpacity onPress={() => navigation.navigate("Splash")}>
+                <Image source={require('../assets/logo_col.png')} style={{
                     width: 50,
                     height: 50,
                     marginRight: 15
                 }} />
             </TouchableOpacity>
-            
+
             <Text style={{
                 color: colors.accentPrimary,
                 fontSize: 30,
@@ -25,7 +27,6 @@ const Header = ({ children, myStyle,navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: colors.back,
         marginTop: 32,
         padding: 20,
         flexDirection: "row",
--- screens/Splash.js
+++ screens/Splash.js
@@ -31,7 +31,7 @@ const Splash = ({ navigation }) => {
     const _renderItem = ({ item, index }) => {
         return (
             <View style={{
-                backgroundColor: colors.back,
+                backgroundColor: theme['background-basic-color-4'],
                 borderRadius: 20,
                 shadowColor: "#000",
                 shadowOffset: {
@@ -50,8 +50,8 @@ const Splash = ({ navigation }) => {
                 alignItems: "center"
             }}>
                 <Image style={styles.bannerImage} source={item.image} />
-                <Text style={{ textAlign: "center", color: colors.text, fontSize: 20, fontWeight: "bold" }}>{item.title}</Text>
-                <Text style={{ textAlign: "center", color: colors.text }}>{item.text}</Text>
+                <Text style={{ textAlign: "center", color: theme['text-basic-color'], fontSize: 20, fontWeight: "bold" }}>{item.title}</Text>
+                <Text style={{ textAlign: "center", color: theme['text-basic-color'] }}>{item.text}</Text>
             </View>
         );
     }
